2f707c6e027ab1424dc27c51272cd87409003613
farestrad <fares610@my.yorku.ca>
2023-04-30 15:16:05 -0400

    Add arrayLists for each level, and one for all levels

--- src/model/Typer.java
+++ src/model/Typer.java
@@ -1,12 +1,22 @@
 package model;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
 public class Typer {
 
 	private JFrame frame;
+	
+	public ArrayList<String> level1 = new ArrayList<>();
+	public ArrayList<String> level2 = new ArrayList<>();
+	public ArrayList<String> level3 = new ArrayList<>();
+	public ArrayList<String> level4 = new ArrayList<>();
+	public ArrayList<String> level5 = new ArrayList<>();
+	public ArrayList<String> level6 = new ArrayList<>();
+	public ArrayList<String> level7 = new ArrayList<>();
+	public ArrayList<String> level8 = new ArrayList<>();
 
 	/**
 	 * Launch the application.

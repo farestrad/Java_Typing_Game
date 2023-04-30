ebab1c2369c93017abde69ed7c11fbeff1e4715d
farestrad <fares610@my.yorku.ca>
2023-04-30 15:00:49 -0400

    Adding swingBuilder

---
+++ src/model/Typer.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Typer {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Typer window = new Typer();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Typer() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  */
@@ -6,4 +7,5 @@
  *
  */
 module typing_Game {
+	requires java.desktop;
 }
